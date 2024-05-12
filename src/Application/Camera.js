77207ed04543bc984991fd1cc1b3fe6031de1809
Soonyoung Park <soon.syp@gmail.com>
2024-05-12 10:34:36 +0200

    add contorls

--- src/Application/Camera.js
+++ src/Application/Camera.js
@@ -1,45 +1,48 @@
 import Application from "./Application";
-import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 
-export default class Camera
-{
-    constructor(){
-        this.application = new Application()
-        this.size = this.application.sizes
-        this.scene = this.application.scene
-        this.canvas = this.application.canvas
+export default class Camera {
+  constructor() {
+    this.application = new Application();
+    this.size = this.application.sizes;
+    this.scene = this.application.scene;
+    this.canvas = this.application.canvas;
 
-        this.setInstance()
-        this.setOrbitControls()
-    }
+    this.setInstance();
+    this.setOrbitControls();
+  }
 
-    setInstance()
-    {
-        this.instance = new THREE.PerspectiveCamera(
-            45,
-            this.size.width / this.size.height,
-            0.1,
-            5000
-        )
-        this.instance.position.set(-10, 10, 10)
-        this.scene.add(this.instance)
-    }
+  setInstance() {
+    this.instance = new THREE.PerspectiveCamera(
+      45,
+      this.size.width / this.size.height,
+      0.1,
+      5000
+    );
+    this.instance.position.set(-10, 10, 10);
+    this.scene.add(this.instance);
+  }
 
-    setOrbitControls()
-    {
-        this.controls = new OrbitControls(this.instance, this.canvas)
-        this.controls.enableDamping = true
-    }
+  setOrbitControls() {
+    this.controls = new OrbitControls(this.instance, this.canvas);
+    this.controls.enableDamping = true;
+    this.controls.dampingFactor = 0.25; //damping inertia
+    this.controls.maxPolarAngle = Math.PI / 2;
+    this.controls.minPolarAngle = 0.1;
+    this.controls.maxAzimuthAngle = 0;
+    this.controls.minAzimuthAngle = -Math.PI / 2;
+    this.controls.minDistance = 10;
+    this.controls.maxDistance = 50;
+    this.controls.enablePan = false;
+  }
 
-    resize()
-    {
-        this.instance.aspect = this.size.width / this.size.height
-        this.instance.updateProjectionMatrix()
-    }
+  resize() {
+    this.instance.aspect = this.size.width / this.size.height;
+    this.instance.updateProjectionMatrix();
+  }
 
-    update()
-    {
-        this.controls.update()
-    }
+  update() {
+    this.controls.update();
+  }
 }
